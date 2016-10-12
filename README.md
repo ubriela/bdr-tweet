a88bfdccb8e8faca5b79acc308c31eb1142d0873
ubriela <ubriela@gmail.com>
2016-10-12 01:20:53 -0700

    clean up

--- README.md
+++ README.md
@@ -10,3 +10,6 @@ https://www.fema.gov/disasters/grid/year/2016?field_disaster_type_term_tid_1=All
 
 6.5 million geotagged twitter posts
 https://github.com/mdredze/twitter_sandy
+
+Library
+SVM classifier: https://github.com/daoudclarke/pysvmlight/tree/master/lib
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import os
 import platform
 from create_vocab_from_tweets import read_data, save_vocab_tweets, make_vocab
 from svm_preprocess import get_vocab, read_train_data, read_test_data, save_data
+from prepare_data import split_data_ryan, split_data_cl
 
 # using proper library depends on OS type
 platform = platform.system()
@@ -15,45 +16,56 @@ else:
     svm_learn = './lib/svm_learn'
     svm_classify = './lib/svm_classify'
 
-# merge crisislex datasets ###############################
+# PARAMETERS ##############################################
+DATASET = "cl"  # CrisisLex
+
+# merge crisislex datasets into one file ###############################
 # os.system("python process_crisislex.py")
 # print "Created training data from CrisisLex26"
 
+# prepare training and testing data ###############################
+
+
+TRAIN, TEST= "./data/training_tweets.txt", "./data/testing_tweets.txt"
+if DATASET == 'cl':
+    INPUT = './data/CrisisLex27K.csv'
+    split_data_cl(INPUT, TRAIN, TEST)
+    labels_map = {'Related and informative': 1, 'Related - but not informative': -1}
+elif DATASET == 'ryan':
+    INPUT = './data/Ryan/10KLabeledTweets.txt'
+    split_data_ryan(INPUT, TRAIN, TEST)
+    labels_map = {'Relevant': 1, 'Not Relevant': -1}
+
+print "Prepared training and testing data"
 
 # create vocabulary ###############################
-# input_filename = "./data/CL_training.csv"
-# output_filename = "./data/CL_refined_training.csv"
-input_filename = "./data/Ryan/training_tweets.txt"
-output_filename = "./data/Ryan/refined_training_tweets.txt"
+output_filename = "./data/refined_training_tweets.txt"
 vocab_filename = "./data/Tweets.vocab"
 
-refined_tweets, save_tweets = read_data(input_filename, tweet_index=4)
+refined_tweets, save_tweets = read_data(TRAIN, tweet_index=4)
 save_vocab_tweets(save_tweets, output_filename)
 make_vocab(refined_tweets, vocab_filename)
-print "Created refined traning data and vocabulary file"
+print "Created refined training data and vocabulary file"
 
 # prepare training and testing inputs ###############################
 # input_train_file="./data/CL_training.csv"
 # input_test_file = "./data/tweets_hashtag.csv" # "./data/output/test.txt" #
-input_train_file, input_test_file="./data/Ryan/training_tweets.txt", "./data/Ryan/testing_tweets.txt"
 test_text_index, test_label_index, train_text_index, train_label_index = 4, 3, 4, 3
-labels_map = {'Relevant':1, 'Not Relevant':-1}
-
-train_file, test_file = "./data/Tweet_Train.txt", "./data/Tweet_Test.txt"
+train_file, test_file, tweet_model = "./data/Tweet_Train.txt", "./data/Tweet_Test.txt", "./output/tweet.model"
 
 tweet_vocab = get_vocab(vocab_filename)
-train_data = read_train_data(input_train_file, tweet_vocab, labels_map, train_text_index, train_label_index, delimiter='\t')
-test_data = read_test_data(input_test_file, tweet_vocab, labels_map, test_text_index, test_label_index, delimiter='\t')
+train_data = read_train_data(TRAIN, tweet_vocab, labels_map, train_text_index, train_label_index, delimiter='\t')
+test_data = read_test_data(TEST, tweet_vocab, labels_map, test_text_index, test_label_index, delimiter='\t')
 
 save_data(train_data, test_data, train_file, test_file)
-print (
-"Prerocess train and test data to output two corresponding files in SVM format: " + train_file + "\t" + test_file)
+print "Preprocess train and test data and output two files in SVM format: " + train_file + "\t" + test_file
 
 ## classify tweets ###############################
-print os.system(svm_learn + " ./data/Tweet_Train.txt ./output/tweet.model")
+print os.system(svm_learn + " " + train_file + " " + tweet_model)
 print "Created model ./output/tweet.model"
-print os.system(svm_classify + " ./data/Tweet_Test.txt ./output/tweet.model ./output/svm_output.txt")
-print "Output classifier result ./output/svm_output.txt"
+svm_output = "./output/svm_output.txt"
+print os.system(svm_classify + " " + test_file + " " + tweet_model + " " + svm_output)
+print "Output classifier result", svm_output
 
 # classify tweets ###############################
 
