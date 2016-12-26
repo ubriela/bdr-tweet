81e382422729978fe71300e59f8608a3d092b4fe
ubriela <hto@usc.edu>
2016-12-25 18:08:05 -0800

    spatial and temporal analysis: many interesting temporal patterns but weak signals on the spatial aspect

---
+++ spatial_stats.py
@@ -0,0 +1,34 @@
+import os.path
+import numpy as np
+import time, datetime, calendar
+from Params import Params
+
+d_duration = Params.disaster_duration
+
+for disaster_id in Params.disaster_ids:
+
+    start_date, end_date = d_duration[disaster_id][0], d_duration[disaster_id][1]
+
+    start_date = calendar.timegm(datetime.datetime.strptime(start_date, "%m-%d-%Y %H:%M:%S").timetuple())
+    end_date = calendar.timegm(datetime.datetime.strptime(end_date, "%m-%d-%Y %H:%M:%S").timetuple())
+
+    # conver from UTC time to PST time
+    start_date, end_date = start_date - 3600 * 8, end_date - 3600 * 8
+
+    # only consider period when disasters happen
+    affected_start_date, affected_end_date = start_date + 3600 * 24, end_date - 3600 * 24
+
+    neg_dict = {}
+    for affect in ['_affected', '_unaffected']:
+        for filter in ['_filtered', '_unfiltered']:
+            filename = disaster_id + affect + filter
+            file = Params.without_tweet_folder + filename + '.txt'
+            if os.path.isfile(file):
+                data = np.loadtxt(file, dtype=int, delimiter='\t', usecols=(1, 5))  # (time, sentiment)
+                neg_rows = [i for i in range(len(data)) if data[i][1] == -1 and affected_start_date < data[i][0] < affected_end_date]
+                all_affected_rows = [i for i in range(len(data)) if affected_start_date < data[i][0] < affected_end_date]
+                # neg_data, all_data = data[neg_rows][:, 0], data[:, 0]
+                neg_dict[affect] = float(len(neg_rows)) / len(all_affected_rows)
+
+    if neg_dict.has_key('_affected') and neg_dict.has_key('_unaffected'):
+        print disaster_id, '\t', round(neg_dict['_affected']-neg_dict['_unaffected'], 2), '\t', neg_dict['_affected'], neg_dict['_unaffected']
---
+++ temporal_stats.py
@@ -0,0 +1,79 @@
+import os.path
+import numpy as np
+import time, datetime, calendar
+
+import matplotlib.path as mpath
+import matplotlib.patches as mpatches
+import matplotlib.pyplot as plt
+
+from Params import Params
+
+d_duration = Params.disaster_duration
+
+for unit_bin in ['day', 'hour']:
+    for graph_type in ['senti_both', 'senti_score']:
+        for disaster_id in Params.disaster_ids:
+            start_date, end_date = d_duration[disaster_id][0], d_duration[disaster_id][1]
+
+            start_date = calendar.timegm(datetime.datetime.strptime(start_date, "%m-%d-%Y %H:%M:%S").timetuple())
+            end_date = calendar.timegm(datetime.datetime.strptime(end_date, "%m-%d-%Y %H:%M:%S").timetuple())
+
+            # conver from UTC time to PST time
+            start_date, end_date = start_date - 3600*8, end_date - 3600*8
+
+            for affect in ['_affected', '_unaffected']:
+                for filter in ['_filtered', '_unfiltered']:
+                    filename = disaster_id + affect + filter
+                    file = Params.without_tweet_folder + filename + '.txt'
+                    print file
+                    if os.path.isfile(file):
+                        data = np.loadtxt(file, dtype=int, delimiter='\t', usecols=(1,5))   # (time, sentiment)
+                        neg_rows = [i for i in range(len(data)) if data[i][1] == -1]
+                        pos_rows = [i for i in range(len(data)) if data[i][1] == 1]
+                        neg_data, pos_data, all_data = data[neg_rows][:,0], data[pos_rows][:,0], data[:,0]
+
+                        if unit_bin == 'hour':
+                            bins = (end_date - start_date)/(3600)
+                        elif unit_bin == 'day':
+                            bins = (end_date - start_date) / (3600*24)
+                        # bins = 10
+                        neg_hist, neg_edges = np.histogram(neg_data, bins)
+                        pos_hist, pos_edges = np.histogram(pos_data, bins)
+                        all_hist, all_edges = np.histogram(all_data, bins)
+
+                        neg_ratio = np.array(neg_hist, dtype=float)/ (all_hist + 0.001)
+
+                        sentiment_score = pos_hist - neg_hist
+
+                        values = sentiment_score
+
+                        # plot graph
+                        fig, ax = plt.subplots()
+
+                        if graph_type == 'senti_score':
+                            index = np.arange(len(values))
+                            bar_width = 0.2
+                            plt.bar(index, values, bar_width, alpha=0.4, color='g', label='Sentiment score')
+                            ticks = [str(i + 1) for i in range(len(values))]
+                            ax.set_xticks(index + bar_width/2)
+                            ax.set_xticklabels(ticks)
+                            ax.set_xlabel('Time (hour)')
+                            ax.set_ylabel('Sentiment score')
+                            ax.legend()
+                            ax.set_title(filename)
+
+                        if graph_type == 'senti_both':
+                            index = np.arange(len(values))
+                            bar_width = 0.2
+                            rects1 = ax.bar(index, neg_hist, bar_width, alpha=0.4, color='r')
+                            rects2 = ax.bar(index + bar_width, pos_hist, bar_width, alpha=0.4, color='y')
+                            ticks = [str(i + 1) for i in range(len(pos_hist))]
+                            ax.set_xticks(index + bar_width)
+                            ax.set_xticklabels(ticks)
+                            ax.set_xlabel('Time (' + unit_bin + ')')
+                            ax.set_ylabel('Sentiment score')
+                            ax.legend((rects1[0], rects2[0]), ('Negative', 'Positive'))
+                            ax.set_title(filename)
+
+                        # plt.show()
+                        plt.savefig('./output/graph/' + graph_type + '_' + unit_bin + '_' + filename + '.png', format='png', dpi=400)
